a8cb01b660680943c749d2c943d40571cbf10a4c
AndresGarciaPerez <andygarcia041995@gmail.com>
2025-01-20 17:40:49 -0600

    modificacion para tamaño de pantallas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-0 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-0 md:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="container mx-auto flex justify-between items-center gap-5">
                 <!-- Logo --> 
@@ -13,7 +13,7 @@
                 </div>
 
                 @guest
-                    <nav class="hidden sm:flex gap-2" > 
+                    <nav class="hidden md:flex gap-2" > 
                         <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('login')}}">Login</a>
                         <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('register')}}">Crear Cuenta</a>
                     </nav>
@@ -21,7 +21,7 @@
 
                 @auth
                     <!-- Navigation Links -->
-                    <nav class="hidden sm:flex gap-5 items-center" > 
+                    <nav class="hidden md:flex gap-5 items-center" > 
                         <a href="{{route('posts.create')}}" 
                         class="flex items-center gap-2 bg-blue-100 border border-blue-300 px-2 py-1 text-blue-800 rounded-lg text-sm uppercase font-bold cursor-pointer">
                         <h1 class="">Crear</h1>
@@ -41,7 +41,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden ml-5 mr-1">
+            <div class="-me-2 flex items-center md:hidden ml-5 mr-1">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -53,7 +53,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden mt-4">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden md:hidden mt-4">
         @auth 
         <nav class="block text-right space-y-4" > 
             <a class="block font-bold text-gray-600 text-sm" href="{{route('posts.index', auth()->user()->username)}}">
